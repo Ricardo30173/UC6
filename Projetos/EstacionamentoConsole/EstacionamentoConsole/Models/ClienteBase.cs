6d60d9a92cd7aba269c041e678bc8dbc507105c9
LUIS RICARDO DA SILVA COSTA <luis.rscosta@senacsp.edu.br>
2025-09-30 22:04:40 -0300

    Projeto 1

---
+++ Projetos/EstacionamentoConsole/EstacionamentoConsole/Models/ClienteBase.cs
@@ -0,0 +1,13 @@
+﻿namespace EstacionamentoConsole.Models
+{
+    public class ClienteBase
+    {
+
+        public string Cpf { get; set; } = null!;
+        public int Id { get; set; }
+
+        public string Nome { get; set; } = null!;
+
+        public string? Telefone { get; set; }
+    }
+}
---
+++ Projetos/EstacionamentoConsole/EstacionamentoConsole/Program.cs
@@ -0,0 +1,74 @@
+﻿using EstacionamentoConsole.Controllers;
+using EstacionamentoConsole.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder(args)
+
+    .ConfigureServices((context, services) =>
+
+    {
+
+        services.AddDbContext<EstacionamentoDbContext>(opt =>
+
+            opt.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=EstacionamentoDB;Trusted_Connection=True;TrustServerCertificate=True;"));
+
+        services.AddTransient<ClienteController>();
+    })
+
+    .Build();
+
+var clienteController = host.Services.GetRequiredService<ClienteController>();
+bool sair = false;
+
+
+
+
+while (!sair) 
+{
+    Console.Clear();
+     
+    Console.WriteLine("===== Sistema de Estacionamento =====");
+    Console.WriteLine("1. Listar clientes ");
+    Console.WriteLine("2. Adicionar cliente ");
+    Console.WriteLine("3. (A FAZER) Gerenciar Veiculos ");
+    Console.WriteLine("4. (A FAZER) Gerenciar vagas ");
+    Console.WriteLine("5. (A FAZER) Ver detalhes do clientes ");
+    Console.WriteLine("0. Sair");   
+
+    string opcao = Console.ReadLine();
+    Console.WriteLine($"Opção escolhida {opcao}");
+
+    switch (opcao)
+    {
+        case "1":
+            clienteController.ListarCliente();
+            break;
+        case "2":
+            clienteController.AdicionarCliente();
+            break;
+        case "3":
+            Console.WriteLine("Chamou o Gerenciar Veiculos");
+            break;
+        case "4":
+            Console.WriteLine("Chamou o Gerenciar vagas");
+            break;
+        case "5":
+           clienteController.VerDetalhesCliente();
+            Console.ReadKey();
+            break;
+        case "0":
+            sair = true;
+            break;
+        default:
+            Console.Clear();
+            Console.WriteLine("Opção inválida. Tente novamente.");
+            Console.ReadKey();
+                 break;
+    }
+}
+Console.WriteLine("Encerrando o sistema. Até logo!");
